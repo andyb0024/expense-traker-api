5fbca12a4a4c335bea81a54e5070106088dfb51b
andyb0024 <andy.benage0024@gmail.com>
2022-04-05 03:34:25 -0700

    Sending Email (Account Activation)

--- ExpenseTrackerApi/settings.py
+++ ExpenseTrackerApi/settings.py
@@ -130,3 +130,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+from.email import (EMAIL_HOST_PASSWORD,EMAIL_HOST_USER)
