3c3691e59369ddab1bf35a9bfc5f3f2e77d29131
andyb0024 <andy.benage0024@gmail.com>
2022-04-22 02:52:24 -0700

    bug fix

--- Expenses/urls.py
+++ Expenses/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('expenses', views.ExpenseListAPIView.as_view(), name="expenses"),
-    path('daily', views.ExpenseByDay.as_view(), name='day-list-api'),
-    path('weekly', views.ExpenseByWeek.as_view(), name='week-list-api'),
+    path('expenses/', views.ExpenseListAPIView.as_view(), name="expenses"),
+    path('daily/', views.ExpenseByDay.as_view(), name='day-list-api'),
+    path('weekly/', views.ExpenseByWeek.as_view(), name='week-list-api'),
     path('income/<int:pk>', views.IncomeListView.as_view(), name="amount-apis"),
     # path('income', views.IncomeListView.as_view(), name='amount-apis'),
 ]
